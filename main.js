0b78c354004bca7d7e2b4493939ad4482a29799c
JericoGatpandan <jericogatpandan0905@gmail.com>
2024-07-04 20:22:56 +0800

    I created the buttons and input to display the expression and result

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-function add() {
-    let num1 = null;
-    let operator = null;
-    let num2 = null;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract() {
-    let num1 = null;
-    let operator = null;
-    let num2 = null;
+function subtract(a, b) {
+    return a - b;
 }
 
-function divide() {
-    let num1 = null;
-    let operator = null;
-    let num2 = null;
+function divide(a, b) {
+    if (b === 0) return 'Error: Divisionby zero';
+    return a / b;
 }
 
-function multiply() {
-    let num1 = null;
-    let operator = null;
-    let num2 = null;
+function multiply(a, b) {
+    return a * b;
+}
+
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-': 
+            return subtract(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        default:
+            return null;
+    }
 }
 
