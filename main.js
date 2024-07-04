f7ce8881014d3a587bbec3116984530c9943affc
JericoGatpandan <jericogatpandan0905@gmail.com>
2024-07-04 19:37:14 +0800

    I start by creating a function for each operation with 3 variable each

---
+++ main.js
@@ -0,0 +1,24 @@
+function add() {
+    let num1 = null;
+    let operator = null;
+    let num2 = null;
+}
+
+function subtract() {
+    let num1 = null;
+    let operator = null;
+    let num2 = null;
+}
+
+function divide() {
+    let num1 = null;
+    let operator = null;
+    let num2 = null;
+}
+
+function multiply() {
+    let num1 = null;
+    let operator = null;
+    let num2 = null;
+}
+
