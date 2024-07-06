e42a0fcb5294c5d811966a6da2b2abe5710aab4f
JericoGatpandan <jericogatpandan0905@gmail.com>
2024-07-06 15:10:35 +0800

    I succesfully complete the code but it still have a help of google, i still dont know how to properly used if else, how to get the value of functions and how to make a connection between function, but i learn how to display the value whereever i want using function

--- main.js
+++ main.js
@@ -29,17 +29,41 @@ function operate(operator, num1, num2) {
             return subtract(num1, num2);
         case '/':
             return divide(num1, num2);
-        case '*':
+        case 'x':
             return multiply(num1, num2);
         default:
             return null;
     }
 }
+const display = document.querySelector('#expression');
 
+function appendDisplay(input) {
+    display.value += input
+}
 
-function appendDisplay(value) {
-    displayValue += value;
-    document.querySelector('#expression').innerText = displayValue;
+function resultDisplay(value){
+    document.querySelector('#result').value = value;
+}
+
+function clearDisplay() {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    displayValue = "";
+    appendDisplay(" ");
+    resultDisplay("0")
+    display.value = " "
+}
+
+function calculate() {
+    if (num1 !== "" && operator !== "" && num2 !== "") {
+        const result = operate(operator, parseFloat(num1), parseFloat(num2));
+        displayValue = result.toString();
+        resultDisplay(displayValue);
+        num1 = displayValue;
+        operator = "";
+        num2 = "";
+    }
 }
 
 // numbers
@@ -50,17 +74,27 @@ document.querySelectorAll('.num').forEach(button => {
         } else {
             num2 += button.innerText;
         }
-        appendDisplay(button.innerText);
+        displayValue = operator === "" ? num1 : num2;
+        appendDisplay(displayValue);
     });
 });
 
 // operator
 document.querySelectorAll('.operator').forEach(button => {
     button.addEventListener('click', () => {
-        if (num1 !== "") {
+        if (num1 !== "" && num2 === "") {
             operator = button.innerText;
-            appendDisplay(button.innerText);
+            displayValue = operator;
+            appendDisplay(displayValue)
+        } else if (num1 !== "" && num2 !== "") {
+            calculate();
+            operator = button.innerText;
+            displayValue = num1 + " " + operator + " ";
+            appendDisplay(displayValue)
+
         }
     });
 });
 
+document.querySelector('.equals').addEventListener('click', calculate);
+document.querySelector('.clear').addEventListener('click', clearDisplay);
